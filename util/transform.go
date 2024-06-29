ba0405ea7e2cb69c985830532db189d4bf57365b
Roihan <hroihan6@gmail.com>
2024-06-29 17:47:21 +0700

    add: ToLocalTime util function

--- util/transform.go
+++ util/transform.go
@@ -1,5 +1,7 @@
 package util
 
+import "time"
+
 func EmailToName(email string) string {
 	idx := 0
 	for i := 0; i < len(email); i++ {
@@ -8,4 +10,17 @@ func EmailToName(email string) string {
 		}
 	}
 	return email[:idx]
+}
+
+func ToLocalTime(t time.Time) time.Time {
+	return time.Date(
+		t.Year(),
+		t.Month(),
+		t.Day(),
+		t.Hour(),
+		t.Minute(),
+		t.Second(),
+		t.Nanosecond(),
+		time.Local,
+	)
 }
