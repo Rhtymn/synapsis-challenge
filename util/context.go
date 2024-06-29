4fd0be165156194bd01ab934696a26728c519a76
Roihan <hroihan6@gmail.com>
2024-06-29 17:48:18 +0700

    add: GetAccountIDFromContext util function

---
+++ util/context.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"context"
+
+	"github.com/Rhtymn/synapsis-challenge/apperror"
+	"github.com/Rhtymn/synapsis-challenge/constants"
+)
+
+func GetAccountIDFromContext(ctx context.Context) (int64, error) {
+	val := ctx.Value(constants.ContextAccountID)
+	id, ok := val.(int64)
+	if !ok {
+		return 0, apperror.NewTypeAssertionFailed(id, val)
+	}
+	return id, nil
+}
