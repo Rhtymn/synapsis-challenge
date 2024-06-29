064861e046bb8481c0e2d601a3576be1dea0c113
Roihan <hroihan6@gmail.com>
2024-06-29 19:24:37 +0700

    add: GetPermissionFromContext util function

--- util/context.go
+++ util/context.go
@@ -15,3 +15,12 @@ func GetAccountIDFromContext(ctx context.Context) (int64, error) {
 	}
 	return id, nil
 }
+
+func GetPermissionFromContext(ctx context.Context) (int64, error) {
+	val := ctx.Value(constants.ContextPermission)
+	permission, ok := val.(int64)
+	if !ok {
+		return 0, apperror.NewTypeAssertionFailed(permission, val)
+	}
+	return permission, nil
+}
