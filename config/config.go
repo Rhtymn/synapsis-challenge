c37b31f2d766ba6a8b066d91b39c847ea6fb1e6f
Roihan <hroihan6@gmail.com>
2024-06-29 15:15:40 +0700

    add: .env new variable

--- config/config.go
+++ config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"errors"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -13,8 +15,15 @@ var (
 
 type Config struct {
 	DatabaseURL string
-	ServerAddr string
-	CorsDomain string
+	ServerAddr  string
+	CorsDomain  string
+
+	JWTIssuer          string
+	SellerAccessSecret string
+	UserAccessSecret   string
+	AdminAccessSecret  string
+
+	AccessTokenLifespan time.Duration
 }
 
 func InitConfig() error {
@@ -23,21 +32,48 @@ func InitConfig() error {
 
 func LoadConfig() (*Config, error) {
 	ret := Config{}
-	
-	if (os.Getenv("DATABASE_URL") == "") {
+
+	if os.Getenv("DATABASE_URL") == "" {
 		return nil, ErrMissingKey
 	}
 	ret.DatabaseURL = os.Getenv("DATABASE_URL")
 
-	if (os.Getenv("SERVER_ADDR") == "") {
+	if os.Getenv("SERVER_ADDR") == "" {
 		return nil, ErrMissingKey
 	}
 	ret.ServerAddr = os.Getenv("SERVER_ADDR")
 
-	if (os.Getenv("CORS_DOMAIN") == "") {
+	if os.Getenv("CORS_DOMAIN") == "" {
 		return nil, ErrMissingKey
 	}
 	ret.CorsDomain = os.Getenv("CORS_DOMAIN")
+
+	if os.Getenv("JWT_ISSUER") == "" {
+		return nil, ErrMissingKey
+	}
+	ret.JWTIssuer = os.Getenv("JWT_ISSUER")
+
+	if os.Getenv("SELLER_ACCESS_SECRET") == "" {
+		return nil, ErrMissingKey
+	}
+	ret.SellerAccessSecret = os.Getenv("SELLER_ACCESS_SECRET")
+
+	if os.Getenv("USER_ACCESS_SECRET") == "" {
+		return nil, ErrMissingKey
+	}
+	ret.UserAccessSecret = os.Getenv("USER_ACCESS_SECRET")
+
+	if os.Getenv("ADMIN_ACCESS_SECRET") == "" {
+		return nil, ErrMissingKey
+	}
+	ret.AdminAccessSecret = os.Getenv("ADMIN_ACCESS_SECRET")
+
+	s := os.Getenv("ACCESS_TOKEN_LIFESPAN")
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return &ret, err
+	}
+	ret.AccessTokenLifespan = time.Duration(i) * time.Minute
 
 	return &ret, nil
 }
