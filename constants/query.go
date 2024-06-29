d15552a21460cc9a593a898a8e64477801a4028f
Roihan <hroihan6@gmail.com>
2024-06-29 23:19:43 +0700

    add: new query constant

--- constants/query.go
+++ constants/query.go
@@ -1,10 +1,12 @@
 package constants
 
 const (
-	UserColumns = ` id, name, photo_url, date_of_birth, gender, phone_number, id_account `
+	UserColumns = ` id, name, photo_url, date_of_birth, gender, phone_number, id_account, main_address_id `
 
 	AccountWithCredentialColumns = ` id, email, email_verified, password, account_type, profile_set `
 	AccountColumns               = ` id, email, email_verified, account_type, profile_set `
 
 	EmailVerifyTokenColumns = ` id, token, expired_at `
+
+	UserAddressColumns = ` id, name, phone_number, address, latitude, longitude, id_user `
 )
