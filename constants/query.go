af65ae12dcaaf5af70502e890bcf73f7796d1091
Roihan <hroihan6@gmail.com>
2024-07-01 02:31:26 +0700

    add: new query constants

--- constants/query.go
+++ constants/query.go
@@ -12,6 +12,17 @@ const (
 
 	ProductJoinedShopColumns = ` p.id, p.name, p.slug, p.photo_url, p.price, p.description, p.stock, p.id_shop, s.shop_name, s.slug `
 
+	PaymentMethodColumns  = ` id, name `
+	ShipmentMethodColumns = ` id, name `
+
+	TransactionColumns = ` id, invoice, status, num_of_items, subtotal, 
+							shipment_fee, total_fee, address, 
+							latitude, longitude, phone_number,
+							id_shop, id_user, id_shipment_method, id_payment_method `
+	TransactionItemColumns = ` id, amount, total_price, id_transaction, id_product `
+
+	PaymentColumns = ` id, file_url, id_transaction `
+
 	SortASC  = "ASC"
 	SortDESC = "DESC"
 )
