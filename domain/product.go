db19dd72d17b97b2b018f6c31eac315c2ffdecb5
Roihan <hroihan6@gmail.com>
2024-06-30 15:47:12 +0700

    add: new domain [category, page, product, seller, shop]

---
+++ domain/product.go
@@ -0,0 +1,38 @@
+package domain
+
+import "context"
+
+const (
+	SortProductByName  = "name"
+	SortProductByPrice = "price"
+)
+
+type Product struct {
+	ID          int64
+	Name        string
+	Slug        string
+	PhotoURL    *string
+	Price       int64
+	Description *string
+	Stock       int64
+	Category    Category
+	Shop        Shop
+}
+
+type ProductQuery struct {
+	Page         int64
+	Limit        int64
+	SortBy       string
+	SortType     string
+	CategorySlug string
+	Search       string
+}
+
+type ProductRepository interface {
+	GetAll(ctx context.Context, query ProductQuery) ([]Product, error)
+	GetPageInfo(ctx context.Context, query ProductQuery) (PageInfo, error)
+}
+
+type ProductService interface {
+	GetAll(ctx context.Context, query ProductQuery) ([]Product, PageInfo, error)
+}
